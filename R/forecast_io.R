aaa199a0bd976a456e488fa7ce7d85cdaafcfbf9
Johnathan Evanilla <jevanilla@bigelow.org>
2021-06-30 15:44:24 -0400

    Added format argument to read_forecast which rounds probabilities and replaces 0 with <1

--- R/forecast_io.R
+++ R/forecast_io.R
@@ -1,10 +1,32 @@
+#' Takes a column of probabilities and formats them for stakeholder delivery (rounds to integer and adds percent symbol)
+#' 
+#' @param probs vector of probabilities
+#' @return formatted vector of probabilities
+#' 
+#' @export
+format_probs <- function(probs) {
+  
+  rounded <- sapply(probs, round)
+  
+  to_char <- sapply(rounded, as.character)
+  
+  no_zero <- sapply(to_char, function(x) if (x == "0") {x = "<1"} else {x})
+  
+  r <- sapply(no_zero, function(x) {return(paste(x, "%", sep=""))})
+  
+  return(r)
+}
+
+
 #' Reads forecast database
 #' 
+#' @param format logical, if true the forecast report will be formatted for stakeholders with rounded probabilities and 0 probabilities being changed to <1
 #' @param new_only logical, if true then only the newest observations from each station will be served
 #' @return tibble of predicted shellfish toxicity classifications along with their metadata
 #' 
 #' @export
-read_forecast <- function(new_only=FALSE) {
+read_forecast <- function(format = FALSE, 
+                          new_only=FALSE) {
   
   file <- system.file("forecastdb/psp_forecast_2021.csv.gz", package="pspforecast")
   
@@ -27,7 +49,20 @@ read_forecast <- function(new_only=FALSE) {
     forecast <- suppressMessages(readr::read_csv(file))
   }
   
-  return(forecast)
+  if (format==TRUE) {
+    forecast <- forecast %>% 
+      dplyr::mutate(prob_0 = format_probs(.data$prob_0),
+                    prob_1 = format_probs(.data$prob_1),
+                    prob_2 = format_probs(.data$prob_2),
+                    prob_3 = format_probs(.data$prob_3)) %>% 
+      dplyr::select(-.data$version,
+                    -.data$class_bins)
+    
+    return(forecast)
+  } else {
+    return(forecast)
+  }
+  
 }
 
 
@@ -39,7 +74,7 @@ write_forecast <- function(predictions) {
   
   file <- file.path("inst/forecastdb/psp_forecast_2021.csv.gz")
   
-  forecast_list %>% readr::write_csv(file, append=TRUE)
+  predictions %>% readr::write_csv(file, append=TRUE)
   
 }
 
@@ -57,60 +92,5 @@ append_forecast <- function(predictions) {
     dplyr::distinct() %>% 
     suppressMessages(readr::write_csv(file))
   
-}
-
-
-#' After making predictions the week prior, add the actual toxicity classification to assess
-#' 
-#' 
-add_forecast_results <- function() {
-  
-  predictions <- pspforecast::read_forecast()
-  
-  tox_levels <- c(0,10,30,80)
-  
-  toxin_measurements <- pspdata::read_psp_data(model_ready=TRUE) %>% 
-    dplyr::mutate(classification = psptools::recode_classification(.data$total_toxicity, tox_levels)) %>% 
-    dplyr::filter(date >= min(predictions$forecast_start_date))
-  
-  
-  find_result <- function(tbl, key) {
-    
-    db <- toxin_measurements %>% 
-      dplyr::filter(.data$location_id == key$location[1]) %>% 
-      dplyr::filter(dplyr::between(date, tbl$forecast_start_date, tbl$forecast_end_date))
-    
-    if (nrow(db) == 0) {
-      
-      empty_results <-   dplyr::tibble(version = character(),
-                                       location = character(),
-                                       date = Sys.Date(),
-                                       measurement_date = Sys.Date(),
-                                       toxicity = numeric(),
-                                       actual_class = numeric())
-      
-      return(empty_results)
-    }
-    
-    forecast_results <- tbl %>% 
-      dplyr::select(.data$version, 
-                    .data$location, 
-                    .data$date) %>% 
-      dplyr::mutate(measurement_date = as.Date(db$date),
-                    toxicity = db$total_toxicity,
-                    actual_class = db$classification)
-    
-    return(forecast_results)
-    
-  }
-  
-  results <- predictions %>%
-    dplyr::group_by(.data$location, 
-                    date) %>% 
-    dplyr::group_map(find_result, .keep=TRUE) %>% 
-    dplyr::bind_rows() %>% 
-    dplyr::arrange(date)
-  
-  return(results)
 }
 
