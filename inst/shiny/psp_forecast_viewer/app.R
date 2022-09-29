3285e8ee5f02b41b190d5efef3e6f735fd3c1301
Johnathan Evanilla <jevanilla@bigelow.org>
2022-09-29 16:13:52 -0400

    Working on shiny interactive forecast results viewer

--- inst/shiny/psp_forecast_viewer/app.R
+++ inst/shiny/psp_forecast_viewer/app.R
@@ -1,119 +1,59 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+
 
 library(shiny)
+library(leaflet)
+
 library(dplyr)
+library(plotly)
+
 library(pspforecast)
 
 
+### Data for shiny app ##
 
-
-#' Make a confusion matrix for predictions made during experimental forecast season
-#' 
-#' @param results a list of predictions from the season matched with actual measurements
-#' @return confusion matrix
-#' 
-#' 
-plot_season <- function(results) {
-    
-    total_predictions <- nrow(results)
-    
-    correct <- results %>% 
-        dplyr::filter(predicted_class == class) %>% 
-        nrow()
-    
-    accuracy <- round(correct/total_predictions, digits=3) *100
-    
-    closures <- results %>% 
-        dplyr::filter(class == 3) %>% 
-        nrow()
-    
-    correct_closures <- results %>% 
-        dplyr::filter(predicted_class == 3 & class == 3) %>% 
-        nrow()
-    
-    fn <- results %>% 
-        dplyr::filter(class !=3 & predicted_class == 3) %>% 
-        nrow()
-    
-    fp <- results %>% 
-        dplyr::filter(class == 3 & predicted_class != 3) %>% 
-        nrow()
-    
-    tp <- correct_closures
-    
-    tn <- results %>% 
-        dplyr::filter(class != 3 & predicted_class != 3) %>% 
-        nrow()
-    
-    sensitivity <- tp/tp+fn
-    
-    specificity <- tn/tn+fp
-    
-    closure_accuracy <- (tp+tn)/(tp+tn+fp+fn)
-    
-    num_levels <- 4
-    levels <- seq(from=0, to=(num_levels-1))
-    
-    cm <- as.data.frame(table(predicted = factor(results$predicted_class, levels), actual = factor(results$class, levels)))
-    
-    confusion_matrix <- ggplot2::ggplot(data = cm,
-                                        mapping = ggplot2::aes(x = .data$predicted, y = .data$actual)) +
-        ggplot2::geom_tile(ggplot2::aes(fill = log(.data$Freq+1))) +
-        ggplot2::geom_text(ggplot2::aes(label = sprintf("%1.0f", .data$Freq)), vjust = 1, size=8) +
-        ggplot2::scale_fill_gradient(low = "white", 
-                                     high = "blue") +
-        ggplot2::labs(x = "Predicted Classifications", 
-                      y = "Actual Classifications", 
-                      title=paste("2021 PSP Forecast Performance"),
-                      subtitle=paste("Accuracy:", accuracy, "%", sep = " "),
-                      caption=paste(Sys.Date())) +
-        ggplot2::theme_linedraw() +
-        ggplot2::theme(axis.text=  ggplot2::element_text(size=14),
-                       axis.title= ggplot2::element_text(size=14,face="bold"),
-                       title =     ggplot2::element_text(size = 14, face = "bold"),
-                       legend.position = "none") 
-    
-    return(confusion_matrix)
-}
-
-predictions <- pspforecast::read_forecast()
 psp <- pspdata::read_psp_data()
 
-results <- add_forecast_results(predictions, psp)
+predictions21 <- read_forecast(season="2021")
+results21 <- add_forecast_results(predictions21, psp) |>
+    mutate(season_week = as.numeric(format(as.Date(date), format="%W"))-14, #need to define number of weeks to offset - different starting week for each season
+           season = 2021) |>
+    relocate(predicted_class, .after=prob_3)
 
-confusion_matrix <- plot_season(results)
+predictions22 <- read_forecast(season="2022")
+results22 <- add_forecast_results(predictions22, psp) |>
+    mutate(season_week = as.numeric(format(as.Date(date), format="%W"))-13,
+           season = 2022) |>
+    rename(prob_0 = p_0,
+           prob_1 = p_1,
+           prob_2 = p_2,
+           prob_3 = p_3) |>
+    select(-ensemble_n, -p3_sd, -p_3_max, -p_3_min)
 
 
-# Define UI for application that draws a histogram
+predictions <- bind_rows(results21, results22)
+
+#confusion_matrix <- plot_season(results)
+
+
+### Shiny App ###
 ui <- fluidPage(
+    titlePanel("Experimental PSP Forecast Results Explorer"),
 
-    # Application title
-    titlePanel("2021 Experimental PSP Forecast"),
-
-    # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
+            selectInput("season",
+                        "Season:",
+                        c("2021", "2022")),
+            sliderInput("week",
+                        "Week of Season:",
                         min = 1,
-                        max = 50,
-                        value = 30)
+                        max = 22,
+                        value = 1)
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           tabsetPanel(
-               tabPanel("Last Forecast", dataTableOutput("last_forecast")),
-               tabPanel("All Forecasts", dataTableOutput("results")),
-               tabPanel("Confusion Matrix", plotOutput("confusion_matrix"))
-           )
+        mainPanel(verticalLayout(
+            leafletOutput("forecast"),
+            plotlyOutput("scatter")
+            )
         )
     )
 )
@@ -121,17 +61,37 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
-    output$last_forecast <- renderDataTable({
-        pspforecast::read_forecast(new_only = TRUE)
-    })
-    output$results <- renderDataTable({
-        add_forecast_results(pspforecast::read_forecast())
-    })
-    output$confusion_matrix <- renderPlot({
-        plot_season(results)
+    output$forecast <- renderLeaflet({
+        results <- predictions |>
+            filter(season == input$season,
+                   season_week == input$week) # How can I only make this call once?
         
+        forecast <- leaflet() |>
+            setView(lng=-68.5,lat = 44, zoom = 7) |>
+            addTiles(group= "Default Background") |>
+            addCircleMarkers(data=results,
+                             lng = ~lon,
+                             lat = ~lat)
     })
-
+    
+    output$scatter <- renderPlotly({
+        results <- predictions |>
+            filter(season == input$season,
+                   season_week == input$week)
+        
+        plot_ly(data=results, 
+                x=~prob_3, 
+                y=~toxicity,
+                #color=~correct,
+                #colors = pal,
+                type="scatter",
+                mode="markers",
+                text=~paste("Location:", location, "<br>", "Date:", date, "<br>", "Predicted Class:", predicted_class, "<br>", "Actual Class", class)) |>
+            layout(#shapes=hline(80),
+                   legend=list(title=list(text='<b> Prediction was correct </b>')),
+                   xaxis = list(title = list(text ='Predicted Probability of Closure-level Toxicity (%)')),
+                   yaxis = list(title = list(text ='Toxocity Measured During Forecast Period (units)')))
+    })
 }
 
 # Run the application 
