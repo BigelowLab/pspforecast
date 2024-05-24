63064c992547c08741f72cb794b362af8ad89c7d
jevanilla <jevanilla@bigelow.org>
2024-05-24 09:25:00 -0700

    Added write_forecast_tables to package for updating current forecast and table for dmr closure map

--- R/dmr_webpage_table.R
+++ R/dmr_webpage_table.R
@@ -7,7 +7,7 @@ format_webpage_table <- function(f) {
   names(levels) <- c(0,1,2,3)
   
   r <- f |>
-    dplyr::select(-dplyr::all_of(c("version", "ensemble_n", "lat", "lon", "class_bins", "p_0", "p_1", "p_2", "p3_sd", "p_3_min", "p_3_max", "f_id"))) |>
+    dplyr::select(-dplyr::all_of(c("version", "ensemble_n", "class_bins", "p_0", "p_1", "p_2", "p3_sd", "p_3_min", "p_3_max", "f_id"))) |>
     dplyr::mutate(p_3 = paste(.data$p_3, "%", sep=""),
                   predicted_class = levels[.data$predicted_class+1]) |>
     dplyr::rename(Location = .data$name,
@@ -16,7 +16,32 @@ format_webpage_table <- function(f) {
                   `Forecast Initialized Date` = .data$date,
                   `Forecast Start Date` = .data$forecast_start_date,
                   `Forecast End Date` = .data$forecast_end_date,
-                  `Closure-level PSP Probability` = .data$p_3)
+                  `Closure-level PSP Probability` = .data$p_3,
+                  Latitude = .data$lat,
+                  Longitude = .data$lon)
   
   return(r)
+}
+
+
+#' Writes two table containing the current forecast. One is formatted for the Interactive Shellfish Closure 
+#' Map and the other has all columns and no formatting.
+#' 
+#' @param new_predictions list with two tibbles of new shellfish toxicity predictions
+#' @param user_config list of pspforecast user configuration containing paths to users local copies of both tables
+#' @returns NULL
+#' @export
+write_forecast_tables <- function(new_predictions, user_config) {
+  
+  t_1 <- read_forecast(new_only=TRUE, id=FALSE) |> 
+    dplyr::arrange(dplyr::desc(.data$p_3))
+  
+  suppressMessages(readr::write_csv(new_predictions$ensemble_forecast, file.path(user_config$output$current_forecast)))
+  
+  t_2 <- read_forecast(new_only=TRUE, id=FALSE) |> 
+    dplyr::arrange(dplyr::desc(.data$p_3)) |>
+    format_webpage_table()
+  
+  suppressMessages(readr::write_csv(format_webpage_table(new_predictions$ensemble_forecast), file.path(user_config$output$dmr_webpage_table)))
+  
 }
