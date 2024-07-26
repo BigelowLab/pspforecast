e5c3a4d0cf8a9cf451531e359319e50e4b155b8e
jevanilla <jevanilla@bigelow.org>
2024-07-26 11:17:32 -0400

    Adding 13 new PSP predictions on 2024-07-26

--- inst/scripts/efi_talk_figures.R
+++ inst/scripts/efi_talk_figures.R
@@ -21,15 +21,15 @@ cm <- as.data.frame(table(predicted = factor(pred_w_results$predicted_class, lev
 
 # res 1400x850
 
-ggplot2::ggplot(data = cm, ggplot2::aes(x = .data$actual, y = .data$predicted)) +
+ggplot2::ggplot(data = cm, ggplot2::aes(x = .data$predicted, y = .data$actual)) +
   ggplot2::geom_tile(ggplot2::aes(fill = log(.data$Freq+1))) +
   ggplot2::geom_text(ggplot2::aes(label = sprintf("%1.0f", .data$Freq)), size=8) +
   ggplot2::facet_grid(cols=vars(.data$year)) +
   #ggplot2::geom_text(ggplot2::aes(label = paste(.data$frac, "%", sep="")), vjust = 3, size=4) +
   ggplot2::scale_fill_gradient(low = "white", 
                                high = "blue") +
-  ggplot2::labs(y = "Predicted Classifications", 
-                x = "Actual Classifications") +
+  ggplot2::labs(x = "Predicted Classifications", 
+                y = "Actual Classifications") +
   ggplot2::theme_linedraw() +
   ggplot2::theme(axis.text=  ggplot2::element_text(size=18),
                  axis.title= ggplot2::element_text(size=18,face="bold"),
@@ -40,12 +40,12 @@ ggplot2::ggplot(data = cm, ggplot2::aes(x = .data$actual, y = .data$predicted)) 
   ggplot2::geom_rect(aes(xmin=3.5, xmax=4.5, ymin=3.5, ymax=4.5), alpha=0)
 
 
-ggplot2::ggplot(data = pred_w_results, ggplot2::aes(x=.data$toxicity, y=.data$p_3)) +
+ggplot2::ggplot(data = pred_w_results, ggplot2::aes(x=.data$p_3, y=.data$toxicity)) +
   ggplot2::geom_point(alpha=0.7, size=3) +
   ggplot2::facet_grid(cols=vars(.data$year)) +
-  ggplot2::labs(y = "Predicted Probability of Closure-level Toxicity (%)",
-                x = "Measured Toxicity (μg STX 100g-1 shellfish)") +
-  ggplot2::geom_vline(xintercept=80, linetype="dashed") +
+  ggplot2::labs(x = "Predicted Probability of Closure-level Toxicity (%)",
+                y = "Measured Toxicity (μg STX 100g-1 shellfish)") +
+  ggplot2::geom_hline(yintercept=80, linetype="dashed") +
   ggplot2::theme_bw() +
   ggplot2::theme(axis.text=  ggplot2::element_text(size=18),
                  axis.title= ggplot2::element_text(size=18,face="bold"),
