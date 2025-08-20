f500df473bb6a31e32453d42b41162f53bf72cae
jevanilla <jevanilla@bigelow.org>
2025-08-19 20:10:46 -0700

    Added brier score to forecast metrics

--- R/forecast_metrics.R
+++ R/forecast_metrics.R
@@ -2,10 +2,13 @@
 #' @param fc tibble of predictions
 #' @param predicted_col character string of predicted classification column name
 #' @param measured_col character string of measured classification column
+#' @param positive_col character stirng of positive (closure-level) class column
+#' @returns one row tibble of metrics
 #' @export
 forecast_metrics <- function(fc, 
                              predicted_col = "predicted_class", 
-                             measured_col = "actual_class") {
+                             measured_col = "actual_class",
+                             positive_col = "prob_3") {
   correct <- fc |>
     dplyr::filter(.data[[predicted_col]] == .data[[measured_col]]) |>
     nrow()
@@ -31,12 +34,17 @@ forecast_metrics <- function(fc,
   cl_accuracy <- (tn+tp)/nrow(fc)
   accuracy <- correct/nrow(fc)
   
+  fc <- dplyr::mutate(fc,
+                      brier_outcome = ifelse(.data[[measured_col]] == 3, 1, 0),
+                      brier_score = ((.data[[positive_col]]/100) - .data$brier_outcome)^2)
+  
   metrics_c3 <- dplyr::tibble(tp = tp,
                               fp = fp,
                               tn = tn,
                               fn = fn,
                               accuracy = accuracy,
                               cl_accuracy = cl_accuracy,
+                              brier = mean(fc$brier_score),
                               f_1=f_1,
                               precision = precision,
                               sensitivity = sensitivity,
